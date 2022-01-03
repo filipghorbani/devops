0e18f2b0c6b972f844889a907923ffa960a64f3b
Filip Ghorbani <filip.ghorbani@consid.se>
2022-01-03 22:59:31 +0100

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          HEJ HEJ
+          HALLÅ
         </p>
         <a
           className="App-link"
